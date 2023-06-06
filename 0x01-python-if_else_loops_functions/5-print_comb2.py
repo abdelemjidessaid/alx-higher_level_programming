5e84c367bb295b5a549027d39ad4e199e7209e1d
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-06-06 17:04:34 +0100

    5-print_comb2.py file pushed up.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 
 for i in range(100):
     if i == 99:
         print("{}".format(i))
     else:
-        print("{}{}, ".format("" if i < 10 else "0", i), end="")
+        print("{}{}, ".format("0" if i < 10 else "", i), end="")
