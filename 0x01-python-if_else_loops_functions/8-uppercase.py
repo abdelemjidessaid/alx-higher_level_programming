4be4fcfdf9f21d28878b0cdbf3619eed47c84b6e
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-06-06 18:26:15 +0100

    8-uppercase.py & 8-main.py files pushed up.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,6 @@
 def uppercase(str):
     for i in str:
         code = ord(i)
-        print("{}".format(chr(code - 32) if code in range(97, 123) else i), end="")
+        char = chr(code - 32) if code in range(97, 123) else i
+        print("{}".format(char), end="")
     print("")
