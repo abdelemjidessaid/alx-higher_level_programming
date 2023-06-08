9e1a78c74fadce96ffcf7abff7c71b1d1c018d45
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-08 13:22:29 +0100

    2-args.py updated.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from sys import argv as ar
 
-length = len(ar)
-status = ""
-if length == 1:
-    status = "s."
-elif length == 2:
-    status = ":"
-else:
-    status = "s:"
-print("{} argument{}".format(length - 1, status))
-if length > 1:
-    for i in range(1, len(ar)):
-        print("{}: {}".format(i, ar[i]))
+if __name__ == "__main__":
+    from sys import argv as ar
+
+    length = len(ar)
+    status = ""
+    if length == 1:
+        status = "s."
+    elif length == 2:
+        status = ":"
+    else:
+        status = "s:"
+    print("{} argument{}".format(length - 1, status))
+    if length > 1:
+        for i in range(1, len(ar)):
+            print("{}: {}".format(i, ar[i]))
