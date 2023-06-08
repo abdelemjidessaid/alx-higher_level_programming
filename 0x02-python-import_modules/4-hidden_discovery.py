e85852051ac9e9903c5808610af9838b5f3970d1
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-08 13:44:45 +0100

    4-hidden_discovery.py file pushed up.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4 as hid
-
     ar = dir(hid)
-    for i in ar:
-        print(i)
+    for name in ar:
+        if name[:2] != '__':
+            print("{}".format(name))
