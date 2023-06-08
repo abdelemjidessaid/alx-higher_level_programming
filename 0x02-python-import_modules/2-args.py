1db16b40f2ff4652226611eb45deb412bfe5c531
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-08 12:50:38 +0100

    2-args.py file pushed up.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv as ar
+
+length = len(ar)
+status = ""
+if length == 1:
+    status = "s."
+elif length == 2:
+    status = ":"
+else:
+    status = "s:"
+print("{} argument{}".format(length - 1, status))
+if length > 1:
+    for i in range(1, len(ar)):
+        print("{}: {}".format(i, ar[i]))
