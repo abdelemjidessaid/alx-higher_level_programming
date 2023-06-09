0eee8f5e0a36beeb40c19ddf0522a098e389bd54
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-09 17:24:13 +0100

    8-main.py & 8-multiple_returns.py files are pushed up.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    first = ""
+    length = 0
+    if sentence == None:
+        first = "None"
+        length = 0
+    else:
+        length = len(sentence)
+        first = "None" if length == 0 else sentence[0]
+    return ((length, first))
+    
