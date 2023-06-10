d0e84230a7a62f382788b0a7d04d4e234d2bfbd7
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-10 12:52:43 +0100

    4-main.py & 4-new_in_list.py files are pushed up, errors are solved.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list == None:
+    if not my_list:
         return (None)
     copy = my_list.copy()
     if idx < 0 or idx > len(copy) - 1:
