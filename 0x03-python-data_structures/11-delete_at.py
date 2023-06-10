94c1a0fad51bcda56b678dc1df9c2ac4c5ca2240
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-10 14:13:23 +0100

    11-delete_at.py & 11-main.py -> files are pushed up

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     ln = len(my_list)
-    if ln < 0 or idx > ln - 1 or ln == 0:
+    if ln < 0 or idx >= ln:
         return (my_list)
     if not my_list:
         return (None)
