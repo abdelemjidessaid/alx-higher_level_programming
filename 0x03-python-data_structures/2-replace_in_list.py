fe2fa10f5a86293bc72a7c5ba21b6de0829effb8
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-10 12:49:44 +0100

    2-replace_in_list.py & 2-main.py files are pushed up, errors about the first condition and the last index are fixed.

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if my_list == None:
+    if not my_list:
         return (None)
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return (my_list)
     my_list[idx] = element
     return (my_list)
