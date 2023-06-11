c256de18d7ddb674b98364660a4946d0c0b78173
abdelemjidessaid <abdelemjidessaid@gmail.com>
2023-06-11 15:17:43 +0100

    11-delete_at.py & 11-main.py -> files are pushed up, errors fixed.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,5 @@ def delete_at(my_list=[], idx=0):
     ln = len(my_list)
     if ln < 0 or idx >= ln:
         return (my_list)
-    if my_list is None:
-        return (None)
     del my_list[idx]
     return (my_list)
