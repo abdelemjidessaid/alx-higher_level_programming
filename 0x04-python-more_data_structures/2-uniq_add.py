3ef7e90d3b3667f6f37e2c154b061375010b1610
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-06-14 15:42:57 +0100

    2-main.py & 2-uniq_add.py -> files are pushed up.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    l = []
     result = 0
-    for i in my_list:
-        if i not in l:
-            l.append(i)
-            result += i
+    unique = set(my_list)
+    for i in unique:
+        result += i
     return (result)
