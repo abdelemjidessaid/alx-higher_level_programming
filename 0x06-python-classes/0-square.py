bf3678f3b03a7bddfb856879752444781ccbb75f
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-06-27 15:42:44 +0100

    Files of task 0 are created & pushed up.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,9 +4,8 @@ class Square:
     """This is an example of class in python.
 
         This class does not do anything, we create it just as a task
-        in program of alx 
+        in program of alx.
 
         Task 0.
     """
     pass
-
