10576406b1b42d68bbba9c21ce57c147aab49c17
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-06-27 15:47:27 +0100

    Files of task 0 are created & pushed up.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+"""Representation of Square"""
+
+
 class Square:
     """This is an example of class in python.
-
-        This class does not do anything, we create it just as a task
-        in program of alx.
-
-        Task 0.
+    This class does not do anything, we create it just as a task
+    in program of alx.
     """
     pass
