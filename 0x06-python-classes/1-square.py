bb8a05829dc015e4339bcd9021ff4f0e51f50741
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-06-27 15:55:09 +0100

    Files of task 1 create and pushed up.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Represent Square as python class"""
+
+
+class Square:
+    """This is a square class, this class required for task 1,
+    in ALX program, to learn python well we need to solve this challenges.
+    """
+    def __init__(self, size):
+        self.__size = size
