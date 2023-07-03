1f0fe9b04a1488e1e271ec2ef6297379fbb4ea9d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-03 20:25:06 +0100

    Files of task 1, are created and pushed.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
                 width (int): initializing the width variable.
                 height (int): initializing the height variable.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
