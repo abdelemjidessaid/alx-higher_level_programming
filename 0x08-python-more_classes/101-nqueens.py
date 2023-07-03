1701b20ba14b1766c509a84b698a3a7863f5f94a
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-03 21:49:35 +0100

    Rest files are created, tested and pushed up successfully.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -46,7 +46,7 @@ def Queen(m_queen, nqueen):
         return
     m_queen[nqueen] = -1
 
-    while((m_queen[nqueen] < len(m_queen) - 1)):
+    while ((m_queen[nqueen] < len(m_queen) - 1)):
         m_queen[nqueen] += 1
         if isSafe(m_queen, nqueen) is True:
 
