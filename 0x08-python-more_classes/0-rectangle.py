3b2d1f49a99798fa1b4656e7e4d8004b9e6ab305
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-03 19:56:44 +0100

    Files of task 0, are created and pushed.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Class Rectangle is an empty class."""
+
+
+class Rectangle:
+    """Rectangle is an empty class
+        Args: None
+        Attributes: None
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,7 @@
+### 0x08-python-more_classes
+
+##### We are solvin tasks about **classes** in Python.
+
+* `0-rectangle.py` This file contains an empty class `Rectangle` that defines a rectangle.
+
+* 
