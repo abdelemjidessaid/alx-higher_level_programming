8dda7b911801d89f138fb855d0eedc686b52b91c
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-03 20:23:31 +0100

    Files of task 1, are created and pushed.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,17 +24,17 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """This is a setter method that sets a new value to width var.
             Args:
                 width (int): new value that should sets to width var.
             Return: None.
         """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if (width < 0):
+        if (value < 0):
             ValueError("width must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -45,14 +45,14 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """This is a setter method that sets a new value to height var.
             Args:
                 height (int): new value that should sets to height var.
             Return: None.
         """
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if (height < 0):
+        if (value < 0):
             ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
