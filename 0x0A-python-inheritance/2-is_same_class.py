222a8d6da965c6789f444afda9e06a7f8a56d1f8
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-10 17:37:22 +0100

    Files of task 2 are pushed.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,8 @@ def is_same_class(obj, a_class):
     """
     is_same_class Function that returns `True` if obj issubclass of a_class
     """
+    if (obj is None or a_class is None):
+        return (False)
     if (a_class == object):
         return (False)
     return (isinstance(obj, a_class))
