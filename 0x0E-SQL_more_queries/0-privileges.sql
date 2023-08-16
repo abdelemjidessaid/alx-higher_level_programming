9d5bcda261d8b8b2bb2dd0388f78b9fee16d7943
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-08-16 18:52:57 +0100

    task 0 is created and pushed up.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Script that lists all privileges of MYSQL users (user_0d_1 & user_0d_2) on localhost server.
-SHOW GRANTS 'user_0d_1'@'localhost' AND 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
