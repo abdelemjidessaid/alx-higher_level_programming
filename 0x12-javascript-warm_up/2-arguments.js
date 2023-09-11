e21bca13bf92152a637a46b2cee50df8e5216b78
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-11 16:17:39 +0100

    task 2 is created and pushed up.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,10 @@
 const { argv } = require('node:process');
 
 const len = argv.length - 2;
-if (len === 0) console.log('No argument');
-else if (len === 1) console.log('Argument found');
-else console.log('Arguments found');
+console.log(
+    count === 2
+        ? 'No argument'
+        : count === 3
+        ? 'Argument found'
+        : 'Arguments found'
+);
