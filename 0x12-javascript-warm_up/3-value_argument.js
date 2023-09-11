efc7a491d86731352ee4a697a45e3897b2a54194
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-11 16:28:21 +0100

    task 3 is created and pushed up.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-
-if (argv.length === 2) console.log('No argument');
-else console.log(argv[2]);
+console.log(argv.length === 2 ? 'No argument' : argv[2]);
