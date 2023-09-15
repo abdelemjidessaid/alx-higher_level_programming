3404b39cfb9407ebaf13f384539184558d89bcd2
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 09:56:54 +0100

    task 2 is updated.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
 """
-my_filter_state Module
-this program search for a keyword in a table from a database.
-Usage:
-    ./2-my_filter_states mysql_user mysql_passwd mysql_db state_name
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=db_name)
-    cursor = db.cursor()
-    query = f"""
-        SELECT * FROM state
-        WHERE name LIKE BINARY '{state_name}'
-        ORDER BY id ASC"""
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-
-    for r in rows:
-        print(r)
-
-    cursor.close()
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
     db.close()
