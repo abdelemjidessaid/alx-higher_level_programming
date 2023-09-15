c1a25ff8a59c9c95827d721930b718b5d7494172
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 12:42:39 +0100

    task 9 is created.

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!usr/bin/python3
+
+"""
+model_state_filter Module
+program that prints all state that starts with letter 'a'
+in 'hbtn_0e_6_usa' database.
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(State)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
