c281624055b23ae30e7f4897cbca4aa271e3a02b
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 10:04:35 +0100

    task 3 is updated.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-
+"""
+my_safe_filter_states Module
+this program search for a keyword in a table from a database.
+Usage: ./3-my_safe_filter_states <mysql username>\
+                                <mysql password>\
+                                <database name>\
+                                <state name searched>
+"""
 import sys
 import MySQLdb
 
-
-"""
-    my_safe_filter_states Module
-    this program search for a keyword in a table from a database.
-"""
-
-
-def main():
-    """
-        main function is the entry point of program.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf-8")
     cursor = db.cursor()
-    query = "SELECT * FROM state WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name, ))
+    query = "SELECT * FROM state WHERE name LIKE %s ORDER BY id ASC"
+    cursor.execute(query, (sys.argv[4], ))
 
     rows = cursor.fetchall()
 
@@ -32,7 +25,3 @@ def main():
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
