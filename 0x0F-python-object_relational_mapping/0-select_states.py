7f5055724a42d0f08cd7a050636b1458789664e1
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 09:43:17 +0100

    task 0 is updated.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-
-import MySQLdb
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+"""
 import sys
-
-"""
-    select_state Module.
-"""
-
+import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", user=username,
-                         passwd=password, db=db_name, port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
-    rows = cursor.fetchall()
-    for r in rows:
-        print(r)
-
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
