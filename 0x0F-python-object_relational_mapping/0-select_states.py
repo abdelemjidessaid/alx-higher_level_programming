baa3ca9d5827a705f0b6ab70897d06bc81ea73ff
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 09:42:35 +0100

    task 0 is updated.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,7 @@ import sys
 """
 
 
-def main():
-    """
-        main function which is the entry point of program.
-    """
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
@@ -19,14 +16,10 @@ def main():
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=db_name, port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     rows = cursor.fetchall()
     for r in rows:
         print(r)
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
