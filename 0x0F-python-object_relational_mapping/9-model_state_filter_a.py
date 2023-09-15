8096c0c2b2075317c8ea81f189a90097714a3a92
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 12:51:14 +0100

    task 9 is created.

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 """
-model_state_filter Module
-program that prints all state that starts with letter 'a'
-in 'hbtn_0e_6_usa' database.
+    A script that lists all State objects from hbtn_0e_6_usa that conatin
+    the letter a from teh database.
+    Username, password and dbname wil be passed as arguments to the script.
 """
 
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 if __name__ == '__main__':
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}"
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(State)
+    Base.metadata.create_all(engine)
     session = Session()
 
-    states = session.query(State).filter(
-        State.name.like('%a%')
-    ).order_by(State.id).all()
+    states = session.query(State).filter(State.name.ilike('%a%')) \
+                    .order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
