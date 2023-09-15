bc0f4449d002b4831ab8d04e06619d85c5fd9388
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-15 12:50:12 +0100

    task 9 is created.

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,7 @@ from model_state import Base, State
 
 if __name__ == '__main__':
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}"
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True
     )
