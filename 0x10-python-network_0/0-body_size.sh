4ad33469959b66508c96dbf8833de02ce599a0ce
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-28 12:42:32 +0100

    task 0 is created.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # counts the body responses.
-url=$1
-response=$(curl -s -o /dev/null -w "%{size_download}" $url)
-echo "$response"
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
