3c04f860957ee867e45e75231a9d7a554a9a7732
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-29 16:13:58 +0100

    task 6 is created.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that:
+    - takes in a URL
+    - an email address
+    - sends a POST request to the passed URL with the email as a parameter
+    - finally displays the body of the response.
+Usage:
+    ./6-post_email.py <URL> <EMAIL>
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    res = requests.post(url=url, data={'email': email})
+    print(res.text)
