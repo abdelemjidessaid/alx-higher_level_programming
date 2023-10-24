f50bd20dca3109c93832fb2f60748e7dc6510483
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-24 12:46:41 +0100

    task 4 is updated.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films';
-request(url, function (err, res, body) {
-  if (!err) {
-    const data = JSON.parse(body).results;
-    console.log(data.reduce((count, film) => {
-      return film.characters.find((character) => character.endsWith('/18/'))
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
