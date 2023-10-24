c8a8d22a930f486e2aeafccd287e78d5a01c86f1
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-24 12:05:39 +0100

    task 3 is done.

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(url, (error, response, body) => {
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  } else {
+    console.error(error);
+  }
+});
